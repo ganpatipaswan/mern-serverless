1310183b9fdb07ef3fca5e09aa2b59d4f6ffe99a
ganpatipaswan <32195368+ganpatipaswan@users.noreply.github.com>
2026-02-10 03:36:58 +0530

    add jwt authentication in all api

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("./auth");
+const authMiddleware = require("./middleware/authMiddleware");
+
 const dynamoDB = require("./dynamodb");
 const s3 = require("./s3");
 
@@ -18,12 +22,13 @@ app.get("/hello", (req, res) => {
 /* =========================
    GET ALL USERS
 ========================= */
-app.get("/users", async (req, res) => {
-  try {
+  app.get("/users", authMiddleware, async (req, res) => {
+    try{
     const data = await dynamoDB.scan({
       TableName: "Users"
     }).promise();
 
+  
     res.json({
       success: true,
       users: data.Items
@@ -55,6 +60,57 @@ app.post("/users", async (req, res) => {
     res.status(500).json({ success: false, error: err.message });
   }
 });
+app.post("/signup", async (req, res) => {
+  const { userId, name, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await dynamoDB.put({
+      TableName: "Users",
+      Item: {
+        userId,
+        name,
+        email,
+        password: hashedPassword,
+        createdAt: new Date().toISOString()
+      }
+    }).promise();
+
+    res.json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.post("/login", async (req, res) => {
+  const { userId, password } = req.body;
+
+  try {
+    const data = await dynamoDB.get({
+      TableName: "Users",
+      Key: { userId }
+    }).promise();
+
+    if (!data.Item) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, data.Item.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken({
+      userId: data.Item.userId,
+      email: data.Item.email
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 /* =========================
    PROFILE IMAGE UPLOAD
@@ -79,10 +135,11 @@ const upload = multer({
  *  - image (file)
  */
 app.post(
-  "/upload_profile_pic",
+  "/upload_profile_pic",authMiddleware,
   upload.single("image"),
   async (req, res) => {
-    const { userId } = req.body;
+    // const { userId } = req.body;
+    const { userId } = req.user; // from JWT
 
     if (!req.file) {
       return res
